6729f8609617fc03126e029b3fca6b5d551178d9
vvani06 <vvani06@gmail.com>
2023-03-24 21:27:00 +0900

    テストコンテストでCEになった分を修正
    - /judge がデフォルトディレクトとして動く想定にした
    - dub init を /judge 直でやるようにした
    - オフラインでもビルドできることが確認できるようにした

--- dmd.sh
+++ dmd.sh
@@ -1,21 +1,18 @@
 set -evx
 
-apt update
-apt install -y --no-install-recommends curl build-essential ca-certificates libxml2
+sudo apt update
+sudo apt install -y --no-install-recommends curl build-essential ca-certificates libxml2
 
 D_COMPLILER="dmd-2.101.2"
 
 wget https://dlang.org/install.sh -O /tmp/install.sh
-cd /tmp
-chmod +x ./install.sh
-./install.sh install ${D_COMPLILER} 
+chmod +x /tmp/install.sh
+/tmp/install.sh install ${D_COMPLILER} 
 
-ln -s $(./install.sh get-path ${D_COMPLILER}) /usr/local/bin/dmd
-ln -s $(./install.sh get-path --dub ${D_COMPLILER}) /usr/local/bin/dub
+sudo ln -s $(/tmp/install.sh get-path ${D_COMPLILER}) /usr/local/bin/dmd
+sudo ln -s $(/tmp/install.sh get-path --dub ${D_COMPLILER}) /usr/local/bin/dub
 
-cd ~
-dub init main -n
-cd main
+dub init -n
 dub add mir
 dub build
-rm main source/app.d
+rm judge source/app.d
